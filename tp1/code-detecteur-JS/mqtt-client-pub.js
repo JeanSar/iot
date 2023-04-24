62b2f21539814874dceefe4ba9d9cec920bd2a46
Hamza TOUHARDJI <hamza@motema.fr>
2023-04-24 13:23:20 +0200

    structure the project

--- tp1/code-detecteur-JS/mqtt-client-pub.js
+++
@@ -1,9 +0,0 @@
-// Source : https://github.com/mqttjs/MQTT.js
-
-import * as mqtt from "mqtt"  // import everything inside the mqtt module and give it the namespace "mqtt"
-let client = mqtt.connect('mqtt://192.168.78.97:3306') // create a client
-
-client.on('connect', function () {
-  client.publish('test/mytopic', process.argv[2]);
-  client.end();
-})
