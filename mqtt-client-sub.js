9df5ad2399b55f367e24a359284325e5c28a826c
JeanSar <jeansar504@gmail.com>
2023-04-25 12:06:31 +0200

    Ca marche pas

--- mqtt-client-sub.js
+++ mqtt-client-sub.js
@@ -1,16 +1,45 @@
 // Source : https://github.com/mqttjs/MQTT.js
 
 import * as mqtt from "mqtt"  // import everything inside the mqtt module and give it the namespace "mqtt"
-import { Board, Sensor, Motor, Stepper, LCD, Led } from "johnny-five";
 import { SerialPort } from "serialport";
-import { Readline } from "@serialport/parser-readline";
 import Firmata from "firmata";
+
+import five from "johnny-five";
+
+SerialPort.list().then(ports => {
+  const device = ports.reduce((accum, item) => {
+    if (item.manufacturer != null && item.manufacturer.indexOf("Arduino") === 0) {
+      return item;
+    }
+    return accum;
+  }, null);
+  /*
+    The following demonstrates using Firmata
+    as an IO Plugin for Johnny-Five
+   */
+  console.log(device.path)
+  const board = new five.Board({
+    io: new Firmata(device.path)
+  });
+
+  board.on("ready", () => {
+    var lcd = new five.LCD({
+      controller: "PCF8574AT",
+      rows: 2,
+      cols: 16,
+    });
+    lcd.print("Hello");
+  });
+});
+
+//var board = new five.Board();
+
+//const serialPort = new SerialPort("/dev/ttyACM0",{});
+
 
 let client = mqtt.connect('mqtt://192.168.78.97:3306') // create a client
 const topic = 'test/mytopic'
 
-const serialPort = new SerialPort("COM6", { baudRate: 115200 });
-const arduino = new Firmata(serialPort);
 
 client.on('connect', function () {
   client.subscribe(topic, function (err) {
@@ -23,19 +52,4 @@ client.on('connect', function () {
 client.on('message', function (topicR, message) {
   // message is Buffer
   console.log(topicR + ': ', message.toString())
-})
-
-arduino.on("ready", () => {
-  const parser = serialPort.pipe(new Readline({ delimiter: "\r\n" }));
-  parser.on("data", (data) => {
-    board.on("ready", () => {
-      const lcd = new LCD({
-        rows: 2,
-        cols: 16,
-      });
-
-      lcd.clear();
-      lcd.print("bonjour titi");
-    });
-  });
-});
+})  
