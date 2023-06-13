0a23d23b4be7ffea01a9f3b8246ef66cacc6386f
TOUHARDJI HAMZA p1805596 <hamza.touhardji@etu.univ-lyon1.fr>
2023-06-13 16:34:57 +0200

    add random reseach flight

--- mqtt-client-sub.js
+++ mqtt-client-sub.js
@@ -1,10 +1,13 @@
 // Source : https://github.com/mqttjs/MQTT.js
 
-import * as mqtt from "mqtt"  // import everything inside the mqtt module and give it the namespace "mqtt"
-import { SerialPort } from "serialport";
-import Firmata from "firmata";
+// import * as mqtt from "mqtt"  // import everything inside the mqtt module and give it the namespace "mqtt"
+// import { SerialPort } from "serialport";
+// import Firmata from "firmata";
 
-import five from "johnny-five";
+// import five from "johnny-five";
+
+import data from './adl_aeroport_lyon.adlvoloperationnelarrivee.json' assert { type: "json" };
+
 
 SerialPort.list().then(ports => {
   const device = ports.reduce((accum, item) => {
@@ -52,13 +55,31 @@ SerialPort.list().then(ports => {
       // message is Buffer
       var msg = message.toString()
       console.log(topicR + ': ', msg)
+
+      // Je tire une valeur aleatoire entre 0 et la taille du dataset
+      let randomIdVol = Math.floor(Math.random() * data.values.length + 1);
+
+      // Je calcule la taille d'un tier des données des vols 
+      const tierDataSize = Math.floor(data.values.length / 3);
+
       lcd.clear()
       lcd.cursor(0, 0).print(msg)
       console.log("Ancienne position : " + servo.position)
-      let newpos = (servo.position + 90) % (servo.range[1] + 90)
+
+      // Je calcule la nouvelle possition du moteur en fonction de la potition du vol dans le dataset. 
+      // Les 33% premiers vols corespondent au tapi n°1 a la position 0 etc ...
+      let newpos = 0;
+      if (values.indexOf(randomIdVol) < tierDataSize) {
+        newpos = (servo.position + 0) % (servo.range[1] + 0)
+      } else if (values.indexOf(randomIdVol) < 2 * tierDataSize) {
+        newpos = (servo.position + 90) % (servo.range[1] + 90)
+      } else {
+        newpos = (servo.position + 180) % (servo.range[1] + 180)
+      }
+
       console.log("Nouvelle position : " + newpos)
       servo.to(newpos, 1000)
-      lcd.cursor(1, 0).print(newpos)
+      lcd.cursor(1, 0).print(data.values[randomIdVol].airlines_airline_name)
       redLed.toggle();
       greenLed.toggle();
     })
