d422e44756248da7cf894c6b56c498ab397cfef2
MBOUP MOUHAMADOU LAMINE p1922789 <mouhamadou-lamine.mboup@etu.univ-lyon1.fr>
2023-06-19 13:01:21 +0200

    moteur

--- moteur.js
+++ moteur.js
@@ -4,10 +4,9 @@ import * as mqtt from "mqtt"
 const port = "/dev/cu.usbmodem1101"
 const topic = 'data/motor'
 let client = mqtt.connect('mqtt://192.168.78.97:3306') // create a client
-let motorSpeed = 0;
+let motorSpeed = 200;
 let goForward = true;
 let isRunning = false;
-let a = 10;
 
 // Subscribe to topic data/motor to retrieve Speed motor
 client.on('connect', function () {
@@ -25,50 +24,24 @@ let board = new five.Board({
 
 board.on('ready', function() {
   console.log("Ready!");
-  //const directionSwitch = new five.Button(4);
-  //const onOffSwitch = new five.Button(5);
   const motor = new five.Motor({
     pins: {
       pwm: 3,
-      dir: 12
+      dir: 12,
     },
-    //invertPWM: true
+    invertPWM: true
   });
-// POUR TESTER
+
   // allow command line access
   board.repl.inject({
     motor: motor
   });
-/*
-  motor.on("start", () => {
-    console.log(`start: ${Date.now()}`);
-  });
 
-  /!*motor.on("stop", () => {
-    //console.log(`automated stop on timer: ${Date.now()}`);
-    console.log("HERE Stop");
-  });*!/
+  // on commence par une vitesse initiale
+  motorStart()
+  motorForward(motorSpeed)
 
-  motor.on("forward", async () => {
-    //console.log(`forward: ${Date.now()}`);
-
-    // demonstrate switching to reverse after 5 seconds
-    //while (a > 0) {
-      //console.log("Speed courant : ", motor.currentSpeed)
-      //await new Promise(r => setTimeout(r, 2000));
-      board.wait(0, () => motor.forward(280));
-    //}
-  });
-
-  // set the motor going forward full speed
-  console.log("HERE BEFORE FIN")
-  motor.forward(200);
-  console.log("HERE FI")*/
-// FIN TEST
-
-
-  // There are two (2) listeners for SPEED MOTOR
-  // 1 / From the MQTT
+  // adaptation de la vitesse en fonction du message reçu
   client.on('message',  (topicName, message) => {
     // message is Buffer
     let vitesse = JSON.parse(message.toString()).vitesse
@@ -77,8 +50,6 @@ board.on('ready', function() {
     console.log("goForward", goForward)
     isRunning = true;
     motorSpeed = vitesse
-    console.log("isRunning", isRunning)
-    console.log("goForward", goForward)
     if (goForward) {
       console.log("Go forward")
       motorForward(motorSpeed);
